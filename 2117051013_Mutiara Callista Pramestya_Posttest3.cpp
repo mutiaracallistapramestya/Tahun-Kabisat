d418ec7b7e2e40d0360e70b3bb462c62a240d206
Mutiara Callista Pramestya <92364379+mutiaracallistapramestya@users.noreply.github.com>
2021-11-15 07:35:55 -0800

    Add files via upload

---
+++ 2117051013_Mutiara Callista Pramestya_Posttest3.cpp
@@ -0,0 +1,35 @@
+#include <iostream> 
+using namespace std;
+
+//Mutiara Callista Pramestya
+//2117051013
+//Post Test 3
+
+int main () {
+int K;
+	
+	cout<< "Masukkan tahun :" ;
+	cin>> K ;
+	
+	if  (K%400==0) {
+		cout<< "Tahun " <<K<< " adalah tahun Kabisat" ;
+	}else if (K%4 == 0){
+		cout << "Tahun " <<K<< " adalah tahun Kabisat";
+    } else if (K%100==0) {
+        cout<<"Bukan tahun Kabisat" ;
+	} else {
+		cout<<"Bukan tahun Kabisat" ;
+		
+	}
+	
+	
+	return 0;
+	
+}
+
+	
+	
+	
+	
+	
+
